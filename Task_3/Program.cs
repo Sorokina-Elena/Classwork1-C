bbe4da48f3838c0459baefa3a26523781feddce5
«Elena <e.sorokina761@gmail.com>
2023-06-11 22:17:09 +0400

    add Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Напишиете программу, которая будет выдавать название дня недели по заданному номеру:
+
+3 -> среда
+5 -> пятница
+
+*/
+
+Console.Write("Input number");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Dictionary<int, string> number = new Dictionary<int, string>()
+{
+{1, "Monday"},
+{2, "Tuesday"},
+{3, "Wednesday"},
+{4, "Thursday"},
+{5, "Friday"},
+{6, "Saturday"},
+{7, "Sunday"}
+};
+
+if(n > 0 && n < 8)
+{
+    Console.WriteLine(number[n]);
+}
+else
+{
+   Console.WriteLine("Ошибка"); 
+}
